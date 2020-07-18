d29eb6dae9bf41c8a4ebc1296bcc588e44a64e34
Kori <korneliaszabo@kinsta.com>
2020-07-18 19:58:54 +0200

    :sparkles: extract form to its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,13 @@
 import React from "react";
-import useCustomForm from "./hooks/useCustomForm";
+import Form from "./components/Form";
 
 const App = () => {
-  const initialValues = {
-    name: "",
-    occupation: "",
-    age: 0
-  };
-
-  const onSubmit = (values: any) => {
-    console.log(values, 'submitting');
-  };
-
-  const {
-    values,
-    handleSubmit,
-    handleBlur,
-    handleChange,
-    touched,
-    errors
-  } = useCustomForm({ initialValues, onSubmit });
-
-  console.log(values, 'initial')
-
   return (
     <div style={{ textAlign: "center" }}>
       <header>
-        <p>A Simple form</p>
+        <p>A Simple form showcase</p>
       </header>
-      <form onSubmit={handleSubmit}>
-        <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          onChange={handleChange}
-          value={values.name}
-        />
-        <label>Occupation</label>
-        <input
-          type="text"
-          name="occupation"
-          onChange={handleChange}
-          value={values.occupation}
-        />
-        <label>Age</label>
-        <input
-          type="number"
-          name="age"
-          onChange={handleChange}
-          value={values.age}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <Form />
     </div>
   );
 };
