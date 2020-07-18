05e300db2d182d96d120dd6f9486fa19208d7c73
Kori <korneliaszabo@kinsta.com>
2020-07-18 13:58:07 +0200

    :sparkles: add handle change function for dealing with change in our form

--- src/hooks/useCustomForm.tsx
+++ src/hooks/useCustomForm.tsx
@@ -1,11 +1,37 @@
+import * as React from 'react'
 import { useState, useEffect, useRef } from "react";
 
 const useCustomForm = ({ initialValues, onSubmit }) => {
+  //set initial state for custom hook
   const [values, setValues] = useState(initialValues || {});
   const [errors, setErrors] = useState({});
   const [touched, setTouched] = useState({});
-  const [onSubmitting, setOnSubmittinng] = useState<boolean>(false);
+  const [onSubmitting, setOnSubmitting] = useState<boolean>(false);
   const [onBlur, setOnBlur] = useState<boolean>(false);
+
+  const formRendered = useRef(true);
+
+  //wait until the form is rendered - handle side effects
+  useEffect(() => {
+    if (!formRendered.current) {
+      setValues(initialValues);
+      setErrors({});
+      setTouched({});
+      setOnSubmitting(false);
+      setOnBlur(false);
+    }
+    formRendered.current = false;
+  }, [initialValues]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //get the event.target for an element
+    const {
+      target: { name, value }
+    } = event;
+    event.persist();
+    //set state to take previous values and add this new
+    setValues({ ...values, [name]: value });
+  };
 };
 
 export default useCustomForm;
